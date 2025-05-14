9dcfec4f2889f80ab420cb1afc25d0fe80961625
Mohammed <mohumadi1998@gmail.com>
2025-05-14 14:19:24 +0100

    task 9 getUsers

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -314,3 +314,27 @@ describe("DELETE /api/comments/:comment_id", () => {
     })
   })
 })
+
+describe("GET /api/users", () => {
+  test("200: responds with array of users with correct properties", () => {
+    return request(app)
+    .get("/api/users")
+    .expect(200)
+    .then(({body}) => {
+      expect(body.users.length).toBe(4)
+      body.users.forEach((user) => {
+        expect(user).toMatchObject({
+          username: expect.any(String),
+          name: expect.any(String),
+          avatar_url: expect.any(String)
+        })
+      })
+    })
+  })
+})
+
+xdescribe("GET /api/articles (sorting queries", () => {
+  test("200: responds with an array of articles sorted by desc created_at", () => {
+    
+  })
+})
